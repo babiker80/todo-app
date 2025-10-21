5624fd1d0469f99c1c9d80821de7fd47a3c7ebeb
Mof Babi <mbabiker80@yahoo.com>
2025-10-21 14:52:31 +0200

    modifications / bugfixes / CORS-Configuration and alternatives

--- src/main/java/de/infoteam/publicservice/todo_app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/de/infoteam/publicservice/todo_app/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package de.infoteam.publicservice.todo_app.exceptions;
 
 import de.infoteam.publicservice.todo_app.dto.TodoErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
@@ -19,5 +21,17 @@ public class GlobalExceptionHandler {
                 .time(OffsetDateTime.now())
                 .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<TodoErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
+        TodoErrorResponse err = TodoErrorResponse.builder()
+                .details("An unexpected error occurred")
+                .uri(request.getRequestURI())
+                .time(OffsetDateTime.now())
+                .build();
+        return ResponseEntity.internalServerError().body(err);
     }
 }
